85abb2bd8b6a2408c591055d69565edb45824888
Pat Wey <patrickwey@gmail.com>
2016-02-01 13:04:22 -0700

    start work on ideas controller
    * Add responders gem
    * Write 1st controller test
    * Migrate the DB ( :P )

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,2 +1,4 @@
 class Api::V1::IdeasController < ApplicationController
+  def index
+  end
 end
--- test/controllers/api/v1/ideas_controller_test.rb
+++ test/controllers/api/v1/ideas_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class Api::V1::IdeasControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#index returns OK response" do
+    get :index, format: :json
+
+    assert_equal 200, response.status
+  end
 end

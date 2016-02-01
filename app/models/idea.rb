6f631ebcb84724c7cfbe7602592a623e5d681b61
Pat Wey <patrickwey@gmail.com>
2016-02-01 15:19:37 -0700

    Update Idea model
    * Add validations
    * Add enum for quality
    * Test model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,8 @@
 class Idea < ActiveRecord::Base
+  validates :title,   presence:   true,
+                      uniqueness: true
+  validates :body,    presence: true
+  validates :quality, presence: true
+
+  enum quality: %w(swill plausible genius)
 end
--- test/models/idea_test.rb
+++ test/models/idea_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class IdeaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "it is valid" do
+    assert valid_idea.valid?
+  end
+
+  test "it is invalid without a title" do
+    idea = valid_idea
+    idea.title = nil
+
+    refute idea.valid?
+  end
+
+  test "it is invalid without a unique title" do
+    valid_idea.save
+
+    idea = valid_idea
+    idea.body = "New body"
+    idea.quality = 1
+
+    refute idea.valid?
+  end
+
+  test "it is invalid without a body" do
+    idea = valid_idea
+    idea.body = nil
+
+    refute idea.valid?
+  end
+
+  test "it is invalid without a quality" do
+    idea = valid_idea
+    idea.quality = nil
+
+    refute idea.valid?
+  end
+
+  def valid_idea
+    Idea.new(title:   "Prestige Worldwide",
+             body:    "Worldwide...wide...wide",
+             quality: 2)
+  end
 end

c966b81d82ea81124aae85d13c9a4c02e2d55a08
Pat Wey <patrickwey@gmail.com>
2016-02-03 21:28:55 -0700

    Fix bug with body truncation
    * No longer adds '...' multiple times

--- app/assets/javascripts/body_truncator.js
+++ app/assets/javascripts/body_truncator.js
@@ -1,11 +1,21 @@
+function getBody(body) {
+  if (body.length <= 100) {
+    return body;
+  } else {
+    return truncateBody(body);
+  }
+}
+
 function truncateBody(body) {
   var words = body.split(" ");
 
-  return words.reduce(function(truncatedBody, word) {
+  newBody = words.reduce(function(truncatedBody, word) {
     if ((truncatedBody + " " + word).length <= 100) {
       return truncatedBody + " " + word;
     } else {
-      return truncatedBody + "...";
+      return truncatedBody;
     }
   });
+
+  return newBody + "...";
 }
--- spec/javascripts/truncate_body_spec.js
+++ spec/javascripts/truncate_body_spec.js
@@ -1,11 +1,11 @@
 //= require body_truncator
 
-describe('truncateBody', function () {
+describe('getBody', function () {
   var validBody = "laksdjflakdjflakdj flkasdjflkajdflk ajdlkf jdaklf jalkdj flaksdj flakdj lsjk lkfadjflkajdklfa kjssik";
 
   it('returns a 100 char body, unchanged', function () {
     assert.equal(100, validBody.length);
-    assert.equal(truncateBody(validBody), validBody);
+    assert.equal(getBody(validBody), validBody);
   });
 
   it('truncates a 104 char body at the nearest word, and adds "..."', function () {
@@ -13,7 +13,7 @@ describe('truncateBody', function () {
     var result = validBody + "...";
 
     assert.equal(104, invalidBody.length);
-    assert.equal(truncateBody(invalidBody), result);
+    assert.equal(getBody(invalidBody), result);
   });
 
   it('truncates a body at the nearest word, and adds "..." (at under 100 chars)', function () {
@@ -23,6 +23,14 @@ describe('truncateBody', function () {
     var result = shorterValidBody + "..."
 
     assert(shorterValidBody.length < 100);
-    assert.equal(truncateBody(invalidBody), result);
+    assert.equal(getBody(invalidBody), result);
+  });
+
+  it('only adds "..." once if there are multiple words past the 100 char limit', function (){
+    var invalidBody = validBody + " " + "one" + " " + "two"
+
+    var result = validBody + "..."
+
+    assert.equal(getBody(invalidBody), result);
   });
 });
